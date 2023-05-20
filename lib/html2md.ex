d93c429ba9bd987172c29a3acee58f63ff86aaaa
Brendon McLean <brendon@zappistore.com>
2023-05-20 14:07:54 +0200

    Packaging hygiene

--- lib/html2md.ex
+++ lib/html2md.ex
@@ -1,6 +1,18 @@
 defmodule Html2MdEx do
   use Rustler, otp_app: :html2md_ex, crate: "html2ex_nif"
 
+  @moduledoc """
+  HTML2Md_Ex is a Rustler NIF that converts HTML to Markdown.
+  """
+
+  @doc """
+  Convert HTML to Markdown.
+
+  ## Examples
+
+      iex> Html2MdEx.parse_html("<h1>Hello, world!</h1>")
+      "Hello, world!\n=============="
+  """
   @spec parse_html(binary) :: binary
   def parse_html(_html), do: error()
 
--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule Html2mdEx.MixProject do
   def project do
     [
       app: :html2md_ex,
+      description: "Convert HTML to Markdown",
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -21,13 +23,14 @@ defmodule Html2mdEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.25.0"}
+      {:rustler, "~> 0.25.0"},
+      {:ex_doc, "~> 0.18", only: :dev}
     ]
   end
 
   defp package do
     [
-      files: ["lib", "mix.exs", "README.md", "priv", "native", "c_src"], 
+      files: ["lib", "mix.exs", "README.md", "priv", "native", "c_src"],
       maintainers: ["Your Name"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/username/repo"}
